ccbffb5cca698b257221ad9a93086d5f6b034fb2
Kothapelli Kiranmai <116304133+kiranmai17@users.noreply.github.com>
2022-11-21 23:58:27 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,17 @@
+"""
+DATE:21ST NOV 2022
+DAY:MONDAY
+TOPIC:STRING METHODS
+AUTHOR:kiranmai
+"""
+a=["pooja"]
+print(a[0]) #pooja
+print(a[1:]) #[]
+print(a[0][0]) #p
+a[0]='p'
+print(a[0]) #p
+a=["pooja"]
+print(a[0][1]) #o
+print(a[0][2]) #o
+print(a[0][3]) #j
+print(a[0][4]) #a
